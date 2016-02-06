bfca77c1efb74bfd21f10c9cfa8dad23517d9f31
SvenWesterlaken <sven.westerlaken@users.noreply.github.com>
2016-02-06 11:52:31 +0100

    Version 1.2
    Almost done

--- js/common.js
+++ js/common.js
@@ -1,5 +1,29 @@
-$(function() {
+$(window).scroll(function(){
 
-	// $("body").hide();
+  if(wScroll > $('.experience').offset().top - ($(window).height() / 1.2)) {
 
+    $('.overview .xpbar').each(function(i){
+
+      setTimeout(function(){
+        $('.overview .xpbar').eq(i).addClass('shown');
+      }, 200 * (i+1));
+    });
+
+  }
+});
+
+
+
+$(document).ready(function(){
+    $(".indepth").hide();
+    $("button").click(function(){
+        $(".indepth").slideToggle(800,function(){
+            $('.indepthcolumn .xpbar').each(function(i){
+
+              setTimeout(function(){
+                $('.indepthcolumn .xpbar').eq(i).addClass('shown');
+            }, 200 * (i+1));
+            });
+        });
+    });
 });
