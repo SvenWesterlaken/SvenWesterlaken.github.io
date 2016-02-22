093b906e965d081abc8391c363a1fdbffa1b03c7
SvenWesterlaken <sven.westerlaken@users.noreply.github.com>
2016-02-22 18:38:13 +0100

    V1.2
    + About
    + Portfolio (not yet complete)
    - Design & Coding
    
    ~ Rearranged
    ~ Footer updated

--- js/common.js
+++ js/common.js
@@ -1,23 +1,18 @@
-$(window).scroll(function(){
-
-  if(wScroll > $('.experience').offset().top - ($(window).height() / 1.2)) {
-
-    $('.overview .xpbar').each(function(i){
-
-      setTimeout(function(){
-        $('.overview .xpbar').eq(i).addClass('shown');
-      }, 200 * (i+1));
+$(document).ready(function(){
+    $('.portfoliogrid').masonry({
+      itemSelector: '.griditem',
+      columnWidth: 180
+    });
+    $(".imglink").html(function(i, oldHTML) {
+      return oldHTML.replace(/_/g, '<br/>');
     });
 
-  }
-});
+    $('.line').width($('.firstline').width() - $('.plus').outerWidth(true));
 
-
-
-$(document).ready(function(){
     $(".indepth").hide();
-    $("button").click(function(){
-        $(".indepth").slideToggle(800,function(){
+    $(".more").hide();
+    $("#indepth").click(function(){
+        $(".indepth").slideToggle(800,'easeOutQuart',function(){
             $('.indepthcolumn .xpbar').each(function(i){
 
               setTimeout(function(){
@@ -26,4 +21,38 @@ $(document).ready(function(){
             });
         });
     });
+    $("#moreabout").click(function(){
+        $('article.about').slideUp(1400,'easeInOutQuart',function(){
+          $(".more").slideDown(1400,'easeInOutQuart',function(){
+            $(".pictureme").animate({transform: 'scale(1,1)'},900,'easeOutElastic');
+          });
+        });
+    });
+});
+
+var expanded = false;
+$(".aboutcontainer").hide();
+
+$(window).scroll(function(){
+
+  var wScroll = $(this).scrollTop();
+
+  if(wScroll > $('.overview').offset().top - ($(window).height() / 1.2)) {
+
+    $('.overview .xpbar').each(function(i){
+
+      setTimeout(function(){
+        $('.overview .xpbar').eq(i).addClass('shown');
+    }, 500 * (i+1));
+    });
+
+  }
+
+  if(!expanded && wScroll >= $('section.about').offset().top - ($(window).height() - $('footer').outerHeight(true))) {
+
+   expanded = true;
+   $(".aboutcontainer").slideDown(1400,'easeInOutQuart');
+
+  }
+
 });
